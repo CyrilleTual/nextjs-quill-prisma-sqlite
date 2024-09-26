146c002a5ddd63abbcf63b84f0f8921a8f571a11
cyrilletual <cyrille.tual@gmail.com>
2024-09-26 11:16:10 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
- 
+ import prisma from '../lib/prisma';
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany();
   return (
     <main className="flex bg-slate-200 flex-col justify-center items-center min-h-full gap-2">
       <h1>Home</h1>
+      <div className="p-4 flex flex-col gap-y-4">
+         <ul className="flex flex-col gap-y-2">
+        {posts.map((post) => (
+          <li key={post.id}>{post.name}</li>
+        ))}
+      </ul>
+      </div>
+     
     </main>
   );
 }
