469f829b24d32cd2b002ba22dfda50f1fdc4bff9
cyrilletual <cyrille.tual@gmail.com>
2024-09-30 11:31:07 +0200

    css improvements

--- app/read/page.tsx
+++ app/read/page.tsx
@@ -27,10 +27,10 @@ export default function ReadPage() {
     <>
       {posts && posts.length > 0 ? (
         <div className="max-w-4xl mx-auto p-4">
+          <h1 className=" text-center text-3xl font-bold text-gray-800 mb-4">
+            Voici les posts disponibles
+          </h1>
           <div className=" text-center grid grid-cols-1 lg:grid-cols-2 gap-6">
-            <h1 className="text-3xl font-bold text-gray-800 mb-4" >
-              Voici les posts disponibles
-            </h1>
             {posts.map((post) => (
               <div
                 key={post.id}
