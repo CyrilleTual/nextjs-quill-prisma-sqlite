e408a7a646e0d577944a92fc60e59640931e853c
cyrilletual <cyrille.tual@gmail.com>
2024-09-30 11:28:22 +0200

    h1 added

--- app/read/page.tsx
+++ app/read/page.tsx
@@ -27,11 +27,14 @@ export default function ReadPage() {
     <>
       {posts && posts.length > 0 ? (
         <div className="max-w-4xl mx-auto p-4">
+          <h1 className="text-3xl text-center font-bold text-gray-800 mb-4">
+            Voici les posts disponibles
+          </h1>
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
             {posts.map((post) => (
               <div
                 key={post.id}
-                className="flex lg:flex-col lg:items-center justify-around min-w-[220px] h-full mb-6 p-6 border rounded-lg shadow-md bg-white transition-transform transform hover:scale-105"
+                className="flex lg:flex-col lg:items-center justify-around min-w-[220px] h-full mb-6 p-6 border rounded-lg shadow-md bg-white text-gray-800"
               >
                 <div>
                   <h2 className="text-xl font-bold text-gray-800 mb-2 flex-grow-0">
