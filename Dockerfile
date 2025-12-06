20bde6390c9dabdf82925ddf922b202eb4824b24
cyrilletual <cyrille.tual@gmail.com>
2025-12-06 10:26:32 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 
-FROM node:18-alpine AS builder
+FROM node:20-slim AS builder
 WORKDIR /app
+RUN apt-get update && apt-get install -y openssl && rm -rf /var/lib/apt/lists/*
 RUN npm install -g pnpm
 COPY prisma ./prisma
 ENV DATABASE_URL="file:./dev.db"
 ENV NEXT_TELEMETRY_DISABLED=1
 COPY package.json ./
 RUN pnpm install
+RUN npx prisma generate
 COPY . .
 RUN pnpm run build
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,43 +1,7 @@
 #!/bin/sh
 
-# Run Prisma migration
-pnpx prisma migrate deploy
-migration_exit_code=$?
+echo "Running Prisma migrations..."
+npx prisma migrate deploy
 
-# Vérifier si la migration a déjà été appliquée
-echo "Checking if migrations are already applied..."
-
-if [ "$(pnpx prisma migrate status --schema=./prisma/schema.prisma --json | jq '.appliedMigrationNames | length')" -eq "0" ]; then
-  echo "No migrations applied yet. Running migration..."
-  pnpx prisma migrate deploy
-  migration_exit_code=$?
-
-  if [ $migration_exit_code -ne 0 ]; then
-    echo "Prisma migration failed. Exiting..."
-    exit $migration_exit_code
-  fi
-else
-  echo "Migrations are already up to date."
-fi
-
-# Function to check if npm server is ready
-wait_for_server() {
-  while ! nc -z localhost 3000; do
-    sleep 1
-  done
-}
-
-# Run npm dev server
-pnpm start &
-
-# Wait for npm dev server to be ready
-wait_for_server
-
- 
-
-# When Prisma Studio exits, kill the npm dev server
-#kill -TERM $dev_server_pid
-
-# Keep the entrypoint script running
-#exec "$@"
-wait
+echo "Starting Next.js..."
+exec pnpm start
